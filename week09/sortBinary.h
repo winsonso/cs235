2103dcb65bc1ba2f2659bd0178f3e40bd0e6923f
Justin <jdeanwaite@gmail.com>
2016-02-29 18:02:15 -0700

    added week 09 starting files

---
+++ week09/sortBinary.h
@@ -0,0 +1,26 @@
+/***********************************************************************
+ * Module:
+ *    Week 09, Sort Binary
+ *    Brother Helfrich, CS 235
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    This program will implement the Binary Tree Sort
+ ************************************************************************/
+
+#ifndef SORT_BINARY_H
+#define SORT_BINARY_H
+
+#include "bst.h"
+
+/*****************************************************
+ * SORT BINARY
+ * Perform the binary tree sort
+ ****************************************************/
+template <class T>
+void sortBinary(T array[], int num)
+{
+}
+
+
+#endif // SORT_BINARY_H
