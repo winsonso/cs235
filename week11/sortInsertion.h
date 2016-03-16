f621bf8a1d5084f713cab48314b966d7d1451a9e
winsonso <winsonsosingwai319@gmail.com>
2016-03-16 13:42:44 -0600

    insertion sort done

--- week11/sortInsertion.h
+++ week11/sortInsertion.h
@@ -3,7 +3,7 @@
  *    Week 11, Sort Insertion
  *    Brother Helfrich, CS 235
  * Author:
- *    <your name>
+ *    Winson So & Justin Waite
  * Summary:
  *    This program will implement the Insertion Sort
  ************************************************************************/
@@ -20,6 +20,17 @@
 template <class T>
 void sortInsertion(T array[], int num)
 {
+  for (int i = 1; i < num; i++)
+  {
+    T value = array[i];
+    int hole = i;
+    while (hole > 0 && array[hole - 1] > value)
+    {
+      array[hole] = array[hole - 1];
+      hole = hole - 1;
+    }
+    array[hole] = value;
+  }
 }
 
 
