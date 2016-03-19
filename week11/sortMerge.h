6cc7572752c4aef2adce30d753b8224beb7c0878
winsonso <winsonsosingwai319@gmail.com>
2016-03-19 12:52:28 -0600

    unfinished merge sort

--- week11/sortMerge.h
+++ week11/sortMerge.h
@@ -11,6 +11,42 @@
 #ifndef SORT_MERGE_H
 #define SORT_MERGE_H
 
+template <class T>
+void merge(T arrayL[], T arrayR[], T arrayA[])
+{
+  int num = sizeof(arrayA) / sizeof(arrayA[0]);
+  int numL = num / 2;
+  int numR = num - numL;
+  int iL =0; int iR = 0; int iA = 0;
+
+  while(iL < numL && iR < numR)
+  {
+    if(arrayL[iL] == arrayR[iR] || arrayR[iR] > arrayL[iL])
+    {
+      arrayA[iA] = arrayL[iL];
+      iL++;
+    }
+    else
+    {
+      arrayA[iA] = arrayR[iR];
+      iR++;
+    }
+    iA++;
+  }
+  while(numL > iL)
+  {
+    arrayA[iA] = arrayL[iL];
+    iL++;
+    iA++;
+  }
+  while(numR > iR)
+  {
+    arrayA[iA] = arrayR[iR];
+    iR++;
+    iA++;
+  }
+}
+
 /*****************************************************
  * SORT MERGE
  * Perform the merge sort
@@ -18,7 +54,35 @@
 template <class T>
 void sortMerge(T array[], int num)
 {
-}
+
+
+
+
+  if(num < 2) return;
+  int midF = num / 2;
+  int midB = num - midF;
+  T* arrayL=new T [midF];
+  T* arrayR=new T [midB];
+//  T arrayL[midF];
+  // T arrayR[50];
+  //   cout << "HIII :"<<midF<<" "<<midB<<endl;
+
+  for(int i = 0; i < midF; i++)
+  {
+    arrayL[i] = array[i];
+  }
+  for(int i = midF; i < num; i++)
+  {
+    arrayR[i - midF] = array[i];
+  }
+
+  sortMerge(arrayL,midF);
+  sortMerge(arrayR,midB);
+  merge(arrayL,arrayR,array);
+
+
+
+ }
 
 
 #endif // SORT_MERGE_H
