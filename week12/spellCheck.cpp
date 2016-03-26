3e80cb97d8fb443a80772ef657c43dcfe3fb7ee9
winsonso <winsonsosingwai319@gmail.com>
2016-03-26 13:44:15 -0600

    unfinished spellChecker.cpp

--- week12/spellCheck.cpp
+++ week12/spellCheck.cpp
@@ -14,11 +14,52 @@
 #include "spellCheck.h"
 #include "hash.h"
 using namespace std;
+/****************************************
+ * S HASH
+ * A simple hash of strings
+ ****************************************/
+class SHash: public Hash<string>
+{
+public:
+   SHash(int numBuckets)   throw (const char*): Hash<string> (numBuckets) {}
+   SHash(const SHash &rhs) throw (const char*): Hash<string> (rhs)        {}
+
+   int hash (const string &str) const
+   {
+   int sum = 0;
+   for (unsigned int i = 0; i < str.size(); i++)
+      sum += str[i];
+   return sum % capacity();
+   }
+};
 
 /*****************************************
  * SPELL CHECK
  * Prompt the user for a file to spell-check
  ****************************************/
+
 void spellCheck()
 {
+  string fileName;
+  SHash hash(101);
+  cout << "What file do you want to check? ";
+  cin >> fileName;
+  // Read the file
+  // Read the file
+  ifstream fin(filename.c_str());
+
+  if (fin.fail())
+  {
+     cout << "Error reading file.\n";
+     fin.close();
+     return false;
+  }
+
+  string word;
+     while(fin >> word)
+     {
+        hash.insert(word);
+     }
+     fin.close();
+
 }
