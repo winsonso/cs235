d7bbf489f079bab5653b9c7c5acc0df0e8b371a4
justinwaite <justin.waite@paqtrack.com>
2016-03-26 15:17:59 -0600

    added hash testing functions and found a better hash function to use.

--- week12/hash.h
+++ week12/hash.h
@@ -4,33 +4,29 @@
  * Author:
  *    Justin Waite & Winson So
  * Summary:
- *    This file contains the class will store a collection of elements
- *    and be able to perform constant-time insertion and lookup.
+ *    This file contains the hash class which will store a collection of
+ *    elements and be able to perform constant-time insertion and lookup.
  ************************************************************************/
 #ifndef WEEK12_HASH_H
 #define WEEK12_HASH_H
 
 #include "list.h"
-#include <iostream>
 
-using namespace std;
-
-/***********************
- * HASH CLASS
+/**************************************************
  * Hash class definition
- ***********************/
+ **************************************************/
 template<class T>
 class Hash
 {
 public:
    // non-default constructor
-   Hash(int buckets) throw (const char*);
+   Hash(int buckets) throw(const char*);
 
    // copy constructor
-   Hash(const Hash<T> &rhs);
+   Hash(const Hash<T> &rhs) throw(const char*);
 
    // assignment operator
-   Hash<T> &operator=(const Hash<T> &rhs);
+   Hash<T> &operator=(const Hash<T> &rhs) throw(const char*);
 
    // determine if hash is empty;
    bool empty() const { return numItems == 0; }
@@ -47,7 +43,17 @@ public:
    // inserts an item into the hash
    void insert(const T &item);
 
-   virtual int hash(const T & value) const = 0;
+   // virtual hash function to be defined in child class
+   virtual int hash(const T &value) const = 0;
+
+   // used to count the empty lists within the data.
+   int getEmptyBuckets() const;
+
+   // used to count the collisions within the data.
+   int getCollisionCount() const;
+
+   // gets biggest bucket size
+   int getBiggestBucket() const;
 
 private:
    int buckets;
@@ -55,17 +61,17 @@ private:
    List<T> *data;
 };
 
-/**************************
+/**************************************************
  * NON-DEFAULT CONSTRUCTOR
- **************************/
+ **************************************************/
 template<class T>
-Hash<T>::Hash(int buckets) throw (const char*) : buckets(buckets), numItems(0)
+Hash<T>::Hash(int buckets) throw(const char*) : buckets(buckets), numItems(0)
 {
    try
    {
       data = new List<T>[buckets];
    }
-   catch(std::bad_alloc)
+   catch (std::bad_alloc)
    {
       throw "Unable to allocate array.";
    }
@@ -76,48 +82,109 @@ Hash<T>::Hash(int buckets) throw (const char*) : buckets(buckets), numItems(0)
  * Constructs a new Hash, copying the given one.
  **************************************************/
 template<class T>
-Hash<T>::Hash(const Hash<T> &rhs)
+Hash<T>::Hash(const Hash<T> &rhs) throw(const char*) : buckets(rhs.buckets),
+                                                       numItems(rhs.numItems)
 {
-  buckets = rhs.buckets;
-  numItems = rhs.numItems;
-  data = rhs.data;
+   try
+   {
+      data = new List<T>[buckets];
+      for (int i = 0; i < rhs.buckets; i++)
+         data[i] = rhs.data[i];
+   }
+   catch (std::bad_alloc)
+   {
+      throw "Unable to allocate array.";
+   }
 }
 
-/********************************
+/**************************************************
  * ASSIGNMENT OPERATOR
  * Copies one hash into another.
- ********************************/
+ **************************************************/
 template<class T>
-Hash<T>& Hash<T>::operator=(const Hash<T> &rhs)
+Hash<T> &Hash<T>::operator=(const Hash<T> &rhs) throw(const char*)
 {
-  buckets = rhs.buckets;
-  numItems = rhs.numItems;
-  data = rhs.data;
-  return *this;
+   if (rhs.buckets > buckets)
+   {
+      try
+      {
+         data = new List<T>[rhs.buckets];
+      }
+      catch (std::bad_alloc)
+      {
+         throw "Unable to allocate array.";
+      }
+   }
+
+   buckets = rhs.buckets;
+   numItems = rhs.numItems;
+
+   for (int i = 0; i < rhs.buckets; i++)
+      data[i] = rhs.data[i];
+
+   return *this;
 }
 
-/*************************************************
+/**************************************************
  * FIND
  * Returns true if an item is found in the hash,
  * false if it is not.
- *************************************************/
+ **************************************************/
 template<class T>
 bool Hash<T>::find(const T &item) const
 {
-
-    return false;
+   int index = hash(item);
+   for (ListIterator<T> it = data[index].begin();
+        it != data[index].end(); ++it)
+      if (*it == item)
+         return true;
+   return false;
 }
 
-/*************************************************
+/**************************************************
  * INSERT
  * Inserts a new instance of a value into the hash.
- *************************************************/
+ **************************************************/
 template<class T>
 void Hash<T>::insert(const T &item)
 {
+   // don't insert if already there!
+   if (find(item))
+      return;
+
    int index = hash(item);
    data[index].push_front(item);
    numItems++;
+}
+
+template<class T>
+int Hash<T>::getEmptyBuckets() const
+{
+   int total = 0;
+   for (int i = 0; i < buckets; i++)
+      if (data[i].empty())
+         total++;
+   return total;
+}
+
+template<class T>
+int Hash<T>::getCollisionCount() const
+{
+   int total = 0;
+   for (int i = 0; i < buckets; i++)
+      if (!data[i].empty())
+         total += data[i].size() - 1;
+   return total;
+}
+
+template<class T>
+int Hash<T>::getBiggestBucket() const
+{
+   int biggest = 0;
+   for (int i = 0; i < buckets; i++)
+      if (!data[i].empty())
+         biggest = (data[i].size() > biggest) ? data[i].size() : biggest;
+   return biggest;
 }
 
 #endif //WEEK12_HASH_H
--- week12/spellCheck.cpp
+++ week12/spellCheck.cpp
@@ -3,7 +3,7 @@
  *    Week 12, Spell Check
  *    Brother Helfrich, CS 235
  * Author:
- *    <your name here>
+ *    Justin Waite & Winson So
  * Summary:
  *    This program will implement the spellCheck() function
  ************************************************************************/
@@ -11,27 +11,44 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <assert.h>
 #include "spellCheck.h"
 #include "hash.h"
+
 using namespace std;
+
 /****************************************
- * S HASH
+ * STRING HASH
  * A simple hash of strings
  ****************************************/
-class SHash: public Hash<string>
+class SHash : public Hash<string>
 {
 public:
-   SHash(int numBuckets)   throw (const char*): Hash<string> (numBuckets) {}
-   SHash(const SHash &rhs) throw (const char*): Hash<string> (rhs)        {}
+   SHash(int numBuckets)   throw(const char*): Hash<string>(numBuckets) { }
 
-   int hash (const string &str) const
+   SHash(const SHash &rhs) throw(const char*): Hash<string>(rhs) { }
+
+   /*
+    * Hash a string.
+    * Based off the djb2 string hash function
+    * with modifications.
+    */
+   int hash(const string &str) const
    {
-   int sum = 0;
-   for (unsigned int i = 0; i < str.size(); i++)
-      sum += str[i];
-   return sum % capacity();
+      // An arbitrary number to start with. 12 was chosen because it seemed to
+      // bring the number of collisions down by about 10-30 vs other numbers.
+      int hash = 12;
+
+      // Loop through each character in the string to get the char int value
+      // and the bit shift is performed to randomize
+      for (int i = 0; i < str.length(); i++)
+         hash = ((hash << 5) + hash) + (int)str[i];
+
+      return abs(hash) % capacity();
    }
 };
+
+bool loadDictionary(string &filename, SHash &dict);
 
 /*****************************************
  * SPELL CHECK
@@ -40,26 +57,78 @@ public:
 
 void spellCheck()
 {
-  string fileName;
-  SHash hash(101);
-  cout << "What file do you want to check? ";
-  cin >> fileName;
-  // Read the file
-  // Read the file
-  ifstream fin(filename.c_str());
+   string fileName;
 
-  if (fin.fail())
-  {
-     cout << "Error reading file.\n";
-     fin.close();
-     return false;
-  }
+   // 5000 was used for the dictionary's bucket size because it leaves only
+   // 3000 empty buckets but reduces the collisions to 488 and the largest
+   // bucket size is only 4. 4 is trivial to search.
+   SHash dict(5000);
+   cout << "What file do you want to check? ";
+   cin >> fileName;
 
-  string word;
-     while(fin >> word)
-     {
-        hash.insert(word);
-     }
-     fin.close();
+   string dictionaryFileName = "dictionary.txt";
+   loadDictionary(dictionaryFileName, dict);
 
+   cout << "Loaded dictionary:" << endl << "\tSize: " << dict.size()
+      << "\n\tCapacity: " << dict.capacity()
+      << "\n\tEmpty Buckets: " << dict.getEmptyBuckets()
+      << "\n\tCollisions: " << dict.getCollisionCount()
+      << "\n\tBiggest Bucket: " << dict.getBiggestBucket()
+      << endl;
+}
+
+bool loadDictionary(string &filename, SHash &dict)
+{
+   assert(filename != "");
+
+   // Read the file
+   ifstream fin(filename.c_str());
+
+   if (fin.fail())
+   {
+      cout << "Error reading file.\n";
+      fin.close();
+      return false;
+   }
+
+   // Iterate through the words
+   string word;
+   while (!fin.eof() && fin >> word)
+   {
+      dict.insert(word);
+   }
+
+   fin.close();
+   return true;
+}
+
+/*******************************************
+ * READ FILE
+ * Reads a given file and puts any misspelled
+ * words into a list.
+ *******************************************/
+bool readFile(string &filename, SHash dictionary, List<string> &misspelled)
+{
+   assert(filename != "");
+
+   // Read the file
+   ifstream fin(filename.c_str());
+
+   if (fin.fail())
+   {
+      cout << "Error reading file.\n";
+      fin.close();
+      return false;
+   }
+
+   // Iterate through the words
+   string word;
+   while (!fin.eof() && fin >> word)
+   {
+      if (!dictionary.find(word))
+         misspelled.push_back(word);
+   }
+
+   fin.close();
+   return true;
 }
